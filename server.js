49327a0cdb99061480f240ddcdb8d70bff1bca74
Zigy <zigybass@gmail.com>
2019-11-06 17:59:42 -0500

    getting closer. no more html in res

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const axios = require("axios");
 const jwt = require("jsonwebtoken");
 
 const PORT = process.env.PORT || 5000;
@@ -8,10 +9,21 @@ const PORT = process.env.PORT || 5000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/api", (req, res) => {
-    res.json({
-        message: "hello from server"
-    });
+axios.post('/api', {
+    headers: {
+        "Content-Type": "application/json"
+    }
+}, (req, res) => {
+    console.log("test server").then( (response) => {
+        res.json({
+            message: "Hello from server"
+        })
+    }
+    ).catch(function(err) {
+        res.status(500).json({
+          error: err.message
+        });
+      });
 });
 
 app.listen(PORT, console.log("Server live at PORT" + PORT));
